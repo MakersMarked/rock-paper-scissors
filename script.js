2c128e76418b4b08bd3be59f303df6d234fb8875
MakersMarked <127469497+MakersMarked@users.noreply.github.com>
2023-03-22 10:26:39 -0700

    Fix semi-colon placement

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 
+
+
 function getComputerChoice (x) {
-    return Math.floor(Math.random(3) * x)+1};
+    return Math.floor(Math.random(3) * x)+1
+}
 const x = getComputerChoice(3)
-   if (x===3) 
-   {alert("Scissors");}
-    else if (x===2) 
-    {alert("Rock")};
-    else 
-    {alert("Paper")}
+   if (x===3) {
+    alert("Scissors");}
+    else if (x===2) {
+        alert("Rock");}
+   else {
+        alert("Paper")}
    
-console.log(x)
+
+   console.log(x)
