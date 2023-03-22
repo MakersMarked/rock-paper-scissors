5bd7aed78f6afd8146a2e9be6e7d50c10ae5a661
MakersMarked <127469497+MakersMarked@users.noreply.github.com>
2023-03-22 10:14:43 -0700

    Create working Function to get computer's choice

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
-
-function getComputerChoice () {
-    return Math.floor(Math.random(3) *3)+1
-        
-
-
-}
-    console.log(getComputerChoice())
+function getComputerChoice (x) {
+    return Math.floor(Math.random(3) * x)+1};
+const x = getComputerChoice(3)
+   if (x===3) 
+   {alert("Scissors");}
+    else if (x===2) 
+    {alert("Rock")};
+    else 
+    {alert("Paper")}
+   
+console.log(x)
