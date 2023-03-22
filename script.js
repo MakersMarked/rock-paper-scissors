5511b78875f95054ab07f2376bd0fb7ee252530c
MakersMarked <127469497+MakersMarked@users.noreply.github.com>
2023-03-22 14:20:16 -0700

    Complete single round of RPS with computer vs Player

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-
-
-
-
-
-
-
-
-
-
 function getComputerChoice() {
-    const choices = Math.floor(Math.random(3) * 3)+1
+    const choices = Math.floor(Math.random() * 3)+1
     if (choices===3) {
-        console.log("Scissors")}
+        return "Scissors"}
     else if (choices===2) {
-        console.log("Rock")}
+        return "Rock"}
     else {
-        console.log("Paper")}
+        return "Paper"}
  }
-console.log(getComputerChoice())
+
+
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection===computerSelection) {
+        return `It's a tie! You both chose ${playerSelection}` 
+    }
+    else if ((playerSelection === "Paper" && computerSelection === "Rock")||
+        playerSelection === "Scissors" && computerSelection === "Paper"||
+        playerSelection === "Rock" && computerSelection === "Scissors"){
+        return `You win! ${playerSelection} beats ${computerSelection}.`
+    }
+    else if (playerSelection === "Paper" && computerSelection === "Scissors" ||
+        playerSelection === "Rock" && computerSelection === "Paper" ||
+        playerSelection === "Scissors" && computerSelection === "Rock") {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }   
+}
+const playerSelection = "Paper";
+const computerSelection = getComputerChoice()
+
+
+
+console.log(playRound(playerSelection,computerSelection))
